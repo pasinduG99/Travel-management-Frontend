8fbc32a98364d4a6f494f0c386599e95b18218d4
pasinduG99 <it20223380@my.sliit.lk>
2022-03-21 18:10:56 +0530

    Add payment component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import Header from './components/Header';
 
+import CreatePackage from './components/Travel_Packages/AddTravelPackage';
+import AllPackages from './components/Travel_Packages/TravelPackages';
+import PackageBooking from './components/Travel_Packages/PackageBooking';
+import PayForTP from './components/Payment/PayForTP';
+import PaymentConfirm from './components/Payment/PaymentComplete';
+import PaymentView from './components/Payment/PayView';
+import PaymentDetails from  './components/Payment/PaymentDetails';
+
+
+import {BrowserRouter as Router, Route} from "react-router-dom";
 function App() {
   return (
+    <Router>
     <div >
-     <Header/>
-    </div>
+
+   
+     <Route path="/travelpackage/admin/add" exact component ={CreatePackage}></Route>
+     <Route path="/travelpackages" exact component = {AllPackages}></Route>
+     <Route path="/bookingpackage/:id" exact component = {PackageBooking}></Route>
+     <Route path="/payment/add-package/:id" exact component={PayForTP}/>
+     <Route path="/confirm/payment/" exact component={PaymentConfirm}/>
+     <Route path="/payment/view/" exact component={PaymentView}/>
+     <Route path="/payment/details/:id" exact component={PaymentDetails}/>
+    
+
+
+</div>
+
+</Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -33,43 +33,9 @@ function Header() {
               <i class="fas fa-car"></i>Travel Package
             </a>
           </li>
-          <li>
-            <a
-              href="/hotelpackage"
-              style={{ textDecoration: "none", color: "white" }}
-            >
-              <i class="fas fa-bed"></i>Room Package
-            </a>
-          </li>
-          <li>
-            <a href="/all" style={{ textDecoration: "none", color: "white" }}>
-              <i class="fas fa-running"></i>Activity
-            </a>
-          </li>
-          <li>
-            <a
-              href="/equipment"
-              style={{ textDecoration: "none", color: "white" }}
-            >
-              <i class="fas fa-campground"></i>Equipment
-            </a>
-          </li>
-          <li>
-            <a
-              href="/guide/all"
-              style={{ textDecoration: "none", color: "white" }}
-            >
-              <i class="fas fa-home"></i>Guides
-            </a>
-          </li>
-          <li>
-            <a
-              href="/feedback"
-              style={{ textDecoration: "none", color: "white" }}
-            >
-              <i class="fas fa-comments"></i>FeedBack
-            </a>
-          </li>
+          
+          
+          
           <li>
             <a href="/add" style={{ textDecoration: "none", color: "white" }}>
               <i class="fas fa-exclamation-circle"></i>Inquiry
